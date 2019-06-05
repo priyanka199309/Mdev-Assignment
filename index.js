833fbb52c552e5c19be428bc611b015c446d1ec0
gargpriyanka1993 <gargpriyanka199309@gmail.com>
2019-06-05 15:53:52 -0400

    assignment

---
+++ index.js
@@ -0,0 +1,63 @@
+const sqlite3 =require("sqlite3").verbose();
+
+const db=new sqlite3.Database(":memory:");
+db.serialize(function()
+{
+
+  db.run("CREATE TABLE Classroom ( Building TEXT, Room_Number NUMBER,Capacity NUMBER)");
+  db.run("INSERT INTO Classroom VALUES('Packard', 101,500)");
+  db.run("INSERT INTO Classroom VALUES('Painter', 514,10)");
+  db.run("INSERT INTO Classroom VALUES('Taylor', 3128,70)");
+  db.run("INSERT INTO Classroom VALUES('Watson', 100,30)");
+  db.run("INSERT INTO Classroom VALUES('Watson', 120,50)");
+
+  db.each("SELECT Room_Number,Building  FROM Classroom where Capacity>50",function(err,row)
+  {
+     if(err)
+     console.log(err);
+     console.log(row);
+  }   
+  );
+     
+});
+db.serialize(function()
+{
+
+  db.run("CREATE TABLE Department ( Dept_Name TEXT, Building Text,Budget NUMBER)");
+  db.run("INSERT INTO Department VALUES('Biology', 'Watson',90000)");
+  db.run("INSERT INTO Department VALUES('Comp_sci', 'Taylor',100000)");
+  db.run("INSERT INTO Department VALUES('Elec.Eng', 'Taylor',85000)");
+  db.run("INSERT INTO Department VALUES('Finance', 'Painter',1200000)");
+  db.run("INSERT INTO Department VALUES('History', 'Painter',50000)");
+  db.run("INSERT INTO Department VALUES('Music', 'Packard',80000)");
+  db.run("INSERT INTO Department VALUES('Physics', 'Watson',70000)");
+  db.each("SELECT Dept_Name FROM Department where Budget>85000",function(err,row)
+{
+     if(err)
+     console.log(err);
+     console.log(row);
+  }
+  );
+
+  
+  let depts = {}
+  db.each("SELECT DISTINCT Dept_Name, Capacity FROM Department NATURAL JOIN Classroom" , function(err,row){
+
+      if(depts[row.Dept_Name] === undefined)
+          depts[row.Dept_Name]=0;
+
+          depts[row.Dept_Name] += row.Capacity;
+
+  },function(err,count){
+      let keys = Object.keys(depts);
+
+      for( let i=0; i != keys.length; ++i){
+          console.log(keys[i] + ":"+depts[keys[i]]);
+      }
+  });
+     
+});
+
+
+
+
